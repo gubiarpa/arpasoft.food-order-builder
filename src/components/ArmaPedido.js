91005f515dd08b91e0b8e3010aa6958e1228b141
Billy Arredondo <billy.arredondo@pucp.pe>
2022-01-23 03:00:26 -0500

    Revert "Dev"

--- src/components/ArmaPedido.js
+++ src/components/ArmaPedido.js
@@ -1,42 +1,9 @@
-import React, { useState } from 'react';
-import { ElegirPedido } from './ElegirPedido';
+import React from 'react';
 
 export const ArmaPedido = () => {
-
-  const [comidas, setComidas] = useState([
-    'Salchipapa',
-    'Hamburguesa',
-  ]);
-
-  const [comidaSelected, setComidaSelected] = useState(comidas[0]);
-  
-  const [cremas, setCremas] = useState([
-    'Ketchup',
-    'Mostaza',
-    'Mayonesa',
-    'Ají',
-  ]);
-
-  const [cremasSelected, setCremasSelected] = useState([]);
-
-  const handleOnClick = (e) => {
-    const mensaje = `${comidaSelected}, cremas: ${cremasSelected.join(', ')}`;
-    window.open(`https://wa.me/51935598917?text=${mensaje}`);
-  }
-
   return (
     <>
-      <h2>Arma tu pedido</h2>
-      <div>
-        <ElegirPedido
-          comidas={comidas} setComidaSelected={setComidaSelected}
-          cremas={cremas} cremasSelected={cremasSelected} setCremasSelected={setCremasSelected}
-        />
-        {/* <a url="http://wa.me/51935598917?text=I'm">Enviar mensaje</a> */}
-      </div>
-      <div className="block-pedido">
-        <button onClick={handleOnClick}>Hacer pedido</button>
-      </div>
+      <h2>Arma Pedido</h2>
     </>
   );
 }
